ea0c4b4fc3f94b21e4193d1df02e77105295e30c
David Logacho Torres <davidlogacho13@outlook.com>
2024-06-21 07:17:08 -0500

    Create a test with MS and NUnit

---
+++ LibreriaUnitTestMSTest/OperacionMSTest.cs
@@ -0,0 +1,24 @@
+﻿using LibreriaUnitTest;
+
+namespace LibreriaUnitTestMSTest
+{
+    [TestClass]
+    public class OperacionMSTest
+    {
+        [TestMethod]
+        public void SumarNumeros_InputDosNumeros_GetValorCorrecto()
+        {
+            //1. Arrange: Inicializar las variables o componentes que ejecutaran el test
+            Operacion op = new();
+            int numero1Test = 50;
+            int numeor2Test = 69;
+
+            //2. Act: Ejecucion de la Operacion
+            int resultado = op.SumarNumeros(numero1Test, numeor2Test);
+
+            //3. Assert: Comparacion de resultado, propio de UNIT TEST
+            Assert.AreEqual(119, resultado);
+
+        }
+    }
+}
---
+++ LibreriaUnitTestNUnitTest/OperacionNUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace LibreriaUnitTest
+{
+    [TestFixture]
+    public class OperacionNUnitTest
+    {
+        [Test]
+        public void SumarNumeros_InputDosNumeros_GetValorCorrecto()
+        {
+            //1. Arrange: Inicializar las variables o componentes que ejecutaran el test
+            Operacion op = new();
+            int numero1Test = 50;
+            int numeor2Test = 69;
+
+            //2. Act: Ejecucion de la Operacion
+            int resultado = op.SumarNumeros(numero1Test, numeor2Test);
+
+            //3. Assert: Comparacion de resultado, propio de UNIT TEST
+            ClassicAssert.AreEqual(119, resultado);
+
+        }
+    }
+}
